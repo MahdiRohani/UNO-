2e590edf8dde760cbf57d2c3f93d5e12ae4fcf46
Mahdi <m.rohani1381@gmail.com>
2023-01-25 14:54:35 +0330

    feat : User

---
+++ src/org/aut/ce/server/User.java
@@ -0,0 +1,44 @@
+package org.aut.ce.server;
+
+public class User {
+    private String firstName;
+    private int age;
+
+
+    public User(String firstName, int age){
+        this.firstName = firstName;
+        this.age = age;
+    }
+
+
+    public String getFirstName(){
+        return firstName;
+    }
+    private void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+
+
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+
+
+
+    @Override
+    public String toString()
+    {
+        return "Person first name: " + firstName + ",  "
+                +
+                "Person age: " + age + ",  ";
+    }
+
+
+    public void print(){
+        System.out.println(this.toString());
+    }
+}
