f4df3c5899e554ccf675ca106bc015e9a4852420
Mahdi <m.rohani1381@gmail.com>
2023-01-25 14:58:22 +0330

    feat : Bot

---
+++ src/org/aut/ce/client/Bot.java
@@ -0,0 +1,15 @@
+package org.aut.ce.client;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Bot extends Player {
+
+    public Bot(int num)
+    {
+        super("Bot"+num, "Bot"+num, 0);
+    }
+
+
+}
+
---
+++ src/org/aut/ce/server/Bot.java
@@ -0,0 +1,15 @@
+package org.aut.ce.server;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Bot extends Player {
+
+    public Bot(int num)
+    {
+        super("Bot"+num, "Bot"+num, 0);
+    }
+
+
+}
+
