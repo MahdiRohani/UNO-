fc7a06d93859703da87d45dd0bc0bd1e5e39d62d
Mahdi <m.rohani1381@gmail.com>
2023-01-25 11:48:22 +0330

    feat : Color

---
+++ src/org/aut/ce/server/Color.java
@@ -0,0 +1,40 @@
+package org.aut.ce.server;
+
+public enum Color {
+    RED,
+    YELLOW,
+    GREEN,
+    BLUE,
+    BLACK,
+    RESET,
+    WHITE,
+
+
+    BLACK_B,
+    RED_B,
+    YELLOW_B,
+    GREEN_B,
+    BLUE_B,
+    WHITE_B;
+
+    private String colorCodeString;
+    static
+    {
+        BLACK.colorCodeString = "\033[0;30m";
+        RED.colorCodeString = "\033[0;91m";
+        YELLOW.colorCodeString = "\033[0;93m";
+        GREEN.colorCodeString = "\033[0;92m";
+        BLUE.colorCodeString = "\033[0;96m";
+        WHITE.colorCodeString = "\033[0;97m";
+
+        RESET.colorCodeString = "\033[92;40m";
+
+        BLACK_B.colorCodeString = "\033[40m";
+        RED_B.colorCodeString = "\033[101m";
+        YELLOW_B.colorCodeString = "\033[103m";
+        GREEN_B.colorCodeString = "\033[102m";
+        BLUE_B.colorCodeString = "\033[106m";
+        WHITE_B.colorCodeString = "\033[107m";
+
+    }
+}
