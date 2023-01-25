a71b9123b1b1d42f7ad3ee1bca20c75383f164ab
Mahdi <m.rohani1381@gmail.com>
2023-01-25 14:10:10 +0330

    feat : SkipCard

---
+++ src/org/aut/ce/client/SkipCard.java
@@ -0,0 +1,45 @@
+package org.aut.ce.client;
+
+import org.aut.ce.server.Card;
+import org.aut.ce.server.Color;
+
+public class SkipCard extends Card {
+
+
+    public SkipCard(org.aut.ce.server.Color cardColor, int cardCode)
+    {
+        super(20, cardColor, cardCode);
+    }
+
+
+    public String toString(int lineNumber)
+
+    {
+        String cardColorCode = org.aut.ce.server.Color.getColorCodeString(super.getCardColor());
+
+        return switch (lineNumber) {
+            case 1, 7 -> cardColorCode + "•-------•" +
+                    org.aut.ce.server.Color.getColorCodeString(org.aut.ce.server.Color.RESET);
+
+            case 2 -> cardColorCode + "|S      |" +
+                    org.aut.ce.server.Color.getColorCodeString(org.aut.ce.server.Color.RESET);
+
+            case 3, 5 -> cardColorCode + "|       |" +
+                    org.aut.ce.server.Color.getColorCodeString(org.aut.ce.server.Color.RESET);
+
+            case 4 -> cardColorCode + "|S k i p|" +
+                    org.aut.ce.server.Color.getColorCodeString(org.aut.ce.server.Color.RESET);
+
+            case 6 -> cardColorCode + "|      S|" +
+                    org.aut.ce.server.Color.getColorCodeString(org.aut.ce.server.Color.RESET);
+
+            case 8 -> org.aut.ce.server.Color.getColorCodeString(org.aut.ce.server.Color.WHITE) + "code: " + super.getCardCode() +
+                    org.aut.ce.server.Color.getColorCodeString(Color.RESET);
+
+            default -> null;
+        };
+
+    }
+
+}
+
---
+++ src/org/aut/ce/server/SkipCard.java
@@ -0,0 +1,42 @@
+package org.aut.ce.server;
+
+public class SkipCard extends Card{
+
+
+    public SkipCard(Color cardColor, int cardCode)
+    {
+        super(20, cardColor, cardCode);
+    }
+
+
+    public String toString(int lineNumber)
+
+    {
+        String cardColorCode = Color.getColorCodeString(super.getCardColor());
+
+        return switch (lineNumber) {
+            case 1, 7 -> cardColorCode + "•-------•" +
+                    Color.getColorCodeString(Color.RESET);
+
+            case 2 -> cardColorCode + "|S      |" +
+                    Color.getColorCodeString(Color.RESET);
+
+            case 3, 5 -> cardColorCode + "|       |" +
+                    Color.getColorCodeString(Color.RESET);
+
+            case 4 -> cardColorCode + "|S k i p|" +
+                    Color.getColorCodeString(Color.RESET);
+
+            case 6 -> cardColorCode + "|      S|" +
+                    Color.getColorCodeString(Color.RESET);
+
+            case 8 -> Color.getColorCodeString(Color.WHITE) + "code: " + super.getCardCode() +
+                    Color.getColorCodeString(Color.RESET);
+
+            default -> null;
+        };
+
+    }
+
+}
+
