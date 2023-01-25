b9871a2bb5e643b6662147f9152a56bf5be463a2
Mahdi <m.rohani1381@gmail.com>
2023-01-25 19:23:39 +0330

    feat : AuthenticationService

---
+++ src/org/aut/ce/client/AuthenticationService.java
@@ -0,0 +1,30 @@
+package org.aut.ce.client;
+
+public class AuthenticationService {
+
+    private String userName;
+    private String pass;
+
+
+    public void setUserName(String userName){
+        this.userName = userName;
+    }
+
+    public void setPass(String pass){
+        this.pass = pass;
+    }
+
+    public static void checkPass(Player player, String pass){
+        while (true)
+        {
+            if (player.getPlayerPass().equals(pass))
+                break;
+
+        }
+
+    }
+
+
+
+
+}
